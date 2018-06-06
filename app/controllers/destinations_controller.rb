c572f43a3d80a7fa57f46f0b6f316f921899df0c
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-06-06 17:44:30 +0300

    Add an edit action in the Destinations controller

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -2,4 +2,8 @@ class DestinationsController < ApplicationController
   def show
     @destination = Destination.find(params[:id])
   end
+
+  def edit
+    @destination = Destination.find(params[:id])
+  end
 end
